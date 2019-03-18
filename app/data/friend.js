3cc49cdde46b8565be1754b19095611a37d5700d
scbonner <codingbootcamp918@gmail.com>
2019-03-17 23:45:07 -0400

    final project

--- app/data/friend.js
+++ app/data/friend.js
@@ -2,29 +2,17 @@ let friends = [
  
     {
         name: "numOne",
-        photo: "https://itsalwayssunny.fandom.com/wiki/Chardee_MacDennis:_The_Game_of_Games",
+        photo: "https://cp.peoplemedia.com/site/general/pm1897/images/2.jpg",
         scores: [3, 1, 4, 4, 2, 4, 2, 5, 1, 3]
     }, {
         name: "numTwo",
-        photo: "https://itsalwayssunny.fandom.com/wiki/Chardee_MacDennis:_The_Game_of_Games",
-        scores: []
+        photo: "https://cp.peoplemedia.com/site/general/pm1897/images/2.jpg",
+        scores: [3, 1, 3, 4, 5, 1, 2, 1, 1, 3]
     }, {
         name: "numThree",
-        photo: "https://itsalwayssunny.fandom.com/wiki/Chardee_MacDennis:_The_Game_of_Games",
-        scores: []
-    }, {
-        name: "numFour",
-        photo: "https://itsalwayssunny.fandom.com/wiki/Chardee_MacDennis:_The_Game_of_Games",
-        scores: []
-    }, {
-        name: "numFive",
-        photo: "https://itsalwayssunny.fandom.com/wiki/Chardee_MacDennis:_The_Game_of_Games",
-        scores: []
-    }, {
-        name: "numSix",
-        photo: "https://itsalwayssunny.fandom.com/wiki/Chardee_MacDennis:_The_Game_of_Games",
-        scores: []
-    }
+        photo: "https://cp.peoplemedia.com/site/general/pm1897/images/2.jpg",
+        scores: [1, 5, 4, 3, 2, 4, 3, 1, 1, 3]
+    }, 
 ];
     
     module.exports = friends;
--- server.js
+++ server.js
@@ -1,30 +1,37 @@
 // dependencies
-const bodyParser = require('body-parser');
 const express = require('express')
 const path = require('path');
+const friends = require("./app/data/friend")
 const htmlRoutes = require("./app/routing/htmlRoutes")
 const apiRoutes = require("./app/routing/apiRoutes")
 var app = express();
 
 var PORT = process.env.PORT || 8080;
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 
-// parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }))
  
-// parse application/json
-app.use(bodyParser.json())
- 
-app.use(function (req, res) {
-  res.setHeader('Content-Type', 'text/plain')
-  res.write('you posted:\n')
-  res.end(JSON.stringify(req.body, null, 2))
+// app.use(function (req, res) {
+//   res.setHeader('Content-Type', 'text/plain')
+//   res.write('you posted:\n')
+//   res.end(JSON.stringify(req.body, null, 2))
+// })
+
+app.get("/:route", function(req, res) {
+  let route = req.params.route
+  
+  res.sendFile(path.join(__dirname, "./app/public/" + route + ".html"))
 })
+app.get("/api/friends", function(req, res) {
+  return res.json(friends)
 
-
-
-
-
+ } )
+app.post("/api/friends", function(req, res) {
+  let newUser = req.body;
+  friends.push(newUser);
+  res.json(newUser)
+})
 
 
 // listener
